ce60c924d49fc9e9b1073cfe3505c7427f416eae
keedpek <zaharv20004@gmail.com>
2024-11-23 21:13:12 +0300

    burger menu & prices section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Header.module.css'
 import { TELEPHONE_NUMBER } from 'consts/consts';
 import tgIco from 'assets/telegramIcon.svg'
@@ -6,15 +6,21 @@ import viberIco from 'assets/viber.svg'
 import logo from 'assets/logo.svg'
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen((prevState) => !prevState);
+  };
+
   return (
     <header>
       <div className={styles.companyWrapper}>
         <div className={styles.logoContainer}>
-          <img src={logo} alt='logo' />
+          <img className={styles.logo} src={logo} alt='logo' />
           <h1 className={styles.h1}>GRUZOPEREVOZKA</h1>
         </div>
         <div className={styles.vertical} />
-        <span>{TELEPHONE_NUMBER}</span>
+        <span className={styles.phoneNum}>{TELEPHONE_NUMBER}</span>
         <div className={styles.buttons}>
           <a target='_blank' rel='noreferrer' href={`https://t.me/${TELEPHONE_NUMBER}`}>
             <img src={tgIco} alt='telegram'/>
@@ -24,20 +30,25 @@ const Header = () => {
           </a>
         </div>
       </div>
-      <nav>
+      <nav className={`${styles.nav} ${menuOpen ? styles.open : ''}`}>
         <ul className={styles.ul}>
-          <li className={styles.li}>
+          <li className={styles.li} onClick={() => {setMenuOpen(false)}}>
             <a href='#services' className={styles.a}>Услуги</a>
           </li>
-          <li className={styles.li}>
+          <li className={styles.li} onClick={() => {setMenuOpen(false)}}>
             <a href='#prices' className={styles.a}>Цены</a>
           </li>
-          <li className={styles.li}>
+          <li className={styles.li} onClick={() => {setMenuOpen(false)}}>
             <a href='#contacts' className={styles.a}>Контакты</a>
           </li>
         </ul>
       </nav>
-      <button className={styles.burgerBtn}></button>
+      <button 
+        className={`${styles.burgerBtn} ${menuOpen ? styles.open : ''}`} 
+        onClick={toggleMenu}
+      >
+        <span></span>
+      </button>
     </header>
   );
 };
