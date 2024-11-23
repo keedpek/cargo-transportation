2e7cbc897a23140e7a353dbb1d67b41808103c39
keedpek <zaharv20004@gmail.com>
2024-11-24 01:29:57 +0300

    pre-final version for test deploy

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,32 +1,27 @@
 import React from 'react';
 import styles from './Footer.module.css'
-import { TELEPHONE_NUMBER } from 'consts/consts';
 import tgIco from 'assets/telegramIcon.svg'
 import viberIco from 'assets/viber.svg'
+import logo from 'assets/logo.svg'
+import { TELEPHONE_NUMBER } from 'consts/consts';
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.logoWrapper}>
-        <img src='https://avatars.mds.yandex.net/i?id=684fd9f82216e86d706a71abdf4ed552aa8c0fdf-9747317-images-thumbs&n=13' alt='logo' className={styles.logo} />
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <div className={styles.logoContainer}>
+        <img className={styles.logo} src={logo} alt='logo' />
+        <h2 className={styles.h2}>GRUZOPEREVOZKA</h2>
       </div>
 
-      <div className={styles.sectionWrapper}>
-        <h3 className={styles.h3}>Контакты</h3>
-        <div className={styles.contacts}>
-          <p>{TELEPHONE_NUMBER}</p>
-          <p>GRUZOPEREVOZKI@gmail.com</p>
-          <a target='_blank' rel='noreferrer' href='https://t.me/+375333747776'>
-            <img src={tgIco} alt='telegram'/>
-          </a>
-          <a target='_blank' rel='noreferrer' href={`viber://chat?number=375333747776`}>
-            <img src={viberIco} alt='viber'/>
-          </a>
-        </div>
-      </div>
-      <div className={styles.sectionWrapper}>
-        <h3 className={styles.h3}>Ещё чёт</h3>
+      <span className={styles.contacts}>{TELEPHONE_NUMBER}</span>
+
+      <div className={styles.buttons}>
+        <a target='_blank' rel='noreferrer' href='https://t.me/+375333747776'>
+          <img className={styles.icons} src={tgIco} alt='telegram'/>
+        </a>
+        <a target='_blank' rel='noreferrer' href={`viber://chat?number=375333747776`}>
+          <img className={styles.icons} src={viberIco} alt='viber'/>
+        </a>
       </div>
     </footer>
   );
